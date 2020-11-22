14ca73674b1309a4db7e2cca3799189db7e02123
knsanchez0001 <knsanchez@umass.edu>
2020-11-21 22:35:50 -0500

    interest calulator skeleton

--- client/main.js
+++ client/main.js
@@ -120,26 +120,6 @@ function renderPie() {
     });
 }
 
-async function setObjective() {
-    const response = await fetch('/tradeHistory')
-        .catch(function (error) {
-            alert(error);
-        });
-    if (response.ok) {
-        const someJSON = await response.json();
-        const docIds = ["starting-capital", "monthly-addition", "percent-gain", "compounded"];
-        for (let i = 0; i < 4; ++i) {
-            const ele = document.getElementById(docIds[i]);
-            ele.innerText = someJSON[i].quantity;
-        }
-    } else {
-        console.error("Could not retrieve the data from the server.");
-    }
-
-}
-
-setObjective();
-
 async function setStats() {
     const response = await fetch('/tradeHistory')
         .catch(function (error) {
